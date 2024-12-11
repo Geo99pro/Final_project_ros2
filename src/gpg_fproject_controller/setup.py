edf621b2816bfecb35dc6daf24836d5c1675d6ed
geo99pro <lhodonou349@gmail.com>
2024-12-11 20:41:04 -0300

    Initialize gpg_fproject_controller package with core functionality and setup

---
+++ src/gpg_fproject_controller/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'gpg_fproject_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['gpg_fproject_controller/controller.py']),
+        ('share/' + package_name, ['gpg_fproject_controller/reach_goal_node.py']),
+        ('share/' + package_name, ['launch/launch.py']),
+        
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lci',
+    maintainer_email='lhodonou349@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [ 'reach_goal_node = gpg_fproject_controller.reach_goal_node:main',
+                            'controller = gpg_fproject_controller.controller:main'
+        ],
+    },
+)
