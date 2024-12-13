176b01dcea7f3907ce323902b68ef5b33e16cdce
Geo99pro <lolo@aluno.puc-rio.br>
2024-12-13 11:42:47 -0300

    Add ObstacleNode class with image and camera info subscriptions

--- src/gpg_fproject/gpg_fproject/obstacle_node.py
+++ src/gpg_fproject/gpg_fproject/obstacle_node.py
@@ -1,5 +1,20 @@
 import cv2
 import rclpy
+import tf2_ros
 import numpy as np
 from rclpy.node import Node
 from std_msgs.msg import String
+from gpg_fproject.utils import get_shape
+from sensor_msgs.msg import Image, CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+from std_msgs.msg import Float64MultiArray, String
+from geometry_msgs.msg import PointStamped, Vector3Stamped
+
+
+class ObstacleNode(Node):
+    def __init__(self):
+        super().__init__('obstacle_node')
+        self.create_subscription(Image, '/image', self.image_callback, 10)
+        self.create_subscription(CameraInfo, '/camera_info', self.camera_info_callback, 10)
+
+        self.po
