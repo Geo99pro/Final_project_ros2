8131f58881c96847617275f038854093c5500021
Geo99pro <lolo@aluno.puc-rio.br>
2025-02-04 19:18:58 -0300

    Add duplicate obstacle verification in obstacle_memory.py

--- src/gpg_fproject/gpg_fproject/obstacle_memory.py
+++ src/gpg_fproject/gpg_fproject/obstacle_memory.py
@@ -12,15 +12,21 @@ class ObstacleMemoryNode(Node):
         self.publishers = {}
 
         self.memory_size = 5
+        self.duplicate_threshold = 0.05
         self.x_coords, self.y_coords, self.z_coords = [], [], []
 
     def obstacle_callback(self, msg):
         """
         Callback function for the obstacle topic
         """
-        self.x_coords.append(float(msg.point.x))
-        self.y_coords.append(float(msg.point.y))
-        self.z_coords.append(float(msg.point.z))
+        x_get_time_step, y_get_time_step, z_get_time_step = float(msg.point.x), float(msg.point.y), float(msg.point.z)
+        if self.verify_duplicated_obstacle(x_get_time_step, y_get_time_step, z_get_time_step):
+            self.get_logger().info('Obstacle is duplicated in the obstacle memory node')
+            return
+        
+        self.x_coords.append(x_get_time_step)
+        self.y_coords.append(y_get_time_step)
+        self.z_coords.append(z_get_time_step)
 
         if len(self.x_coords) > self.memory_size:
             self.x_coords.pop(0)
@@ -30,16 +36,29 @@ class ObstacleMemoryNode(Node):
         self.get_logger().info('Obstacle position received in the obstacle memory node')
         self.publish_obstacle_memory()
 
+    def verify_duplicated_obstacle(self, x_get_time_step, y_get_time_step, z_get_time_step):
+        """
+        Verify if the obstacle is already in the memory
+        """
+        if not self.x_coords:
+            return False
+        
+        distance_eucluiden = np.sqrt((x_get_time_step - self.x_coords)**2 + 
+                                     (y_get_time_step - self.y_coords)**2 + 
+                                     (z_get_time_step - self.z_coords)**2)
+        return np.any(distance_eucluiden < self.duplicate_threshold)
+
     def publish_obstacle_memory(self):
         """
         Publish the obstacle memory
         """
         obstacle_msg = UnboundedFloat()
         obstacle_msg.header.stamp = self.get_clock().now().to_msg()
-        obstacle_msg.float_storage = np.array([self.x_coords, self.y_coords, self.z_coords]).T.tolist() # convert matrix to list of lists with each list containing x, y, z coordinates
+        obstacle_msg.float_storage  = [val for sublist in zip(self.x_coords, self.y_coords, self.z_coords) for val in sublist]
         self.memory_publisher.publish(obstacle_msg)
         
-        for i, obs_coords in enumerate(obstacle_msg.float_storage): # obs_coords is a 1D array of x, y, z coordinates
+        arraye_format = np.array(obstacle_msg.float_storage).reshape(-1, 3)
+        for i, obs_coords in enumerate(arraye_format): 
             topic_name = f'/obstacle_position_{i}'
             if topic_name not in self.publishers:
                 self.publishers[topic_name] = self.create_publisher(PointStamped, topic_name, 10)
@@ -51,7 +70,7 @@ class ObstacleMemoryNode(Node):
             try:
                 self.publishers[topic_name].publish(PS)
             except Exception as e:
-                self.get_logger().error(f'Error publishing obstacle to topic {topic_name}: {e}')
+                self.get_logger().error(f'Error publishing obstacle to topic {topic_name}: {e}')           
 
 def main(args=None):
     rclpy.init(args=args)
@@ -62,7 +81,19 @@ def main(args=None):
 
 
 
+# for i, obs_coords in enumerate(obstacle_msg.float_storage[:3]): 
+        #     topic_name = f'/obstacle_position_{i}'
+        #     if topic_name not in self.publishers:
+        #         self.publishers[topic_name] = self.create_publisher(PointStamped, topic_name, 10)
+            
+        #     PS = PointStamped()
+        #     PS.header.stamp = self.get_clock().now().to_msg()
+        #     PS.point.x, PS.point.y, PS.point.z = obs_coords
 
+        #     try:
+        #         self.publishers[topic_name].publish(PS)
+        #     except Exception as e:
+        #         self.get_logger().error(f'Error publishing obstacle to topic {topic_name}: {e}')
 
 
 
