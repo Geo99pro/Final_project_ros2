dc4449ae11cd2345bda632b1b49a45dc7df0b665
geo99pro <lhodonou349@gmail.com>
2025-02-11 21:54:54 -0300

    Refactor obstacle_memory.py to improve variable naming and enhance code clarity

--- src/gpg_fproject/gpg_fproject/obstacle_memory.py
+++ src/gpg_fproject/gpg_fproject/obstacle_memory.py
@@ -9,7 +9,7 @@ class ObstacleMemoryNode(Node):
         super().__init__('obstacle_memory_node')
         self.create_subscription(PointStamped, '/obstacle_position', self.obstacle_callback, 10)
         self.memory_publisher = self.create_publisher(UnboundedFloat, '/obstacle_memory', 10)
-        self.publishers = {}
+        self.obstacle_publishers = {} 
 
         self.memory_size = 5
         self.duplicate_threshold = 0.05
@@ -43,10 +43,12 @@ class ObstacleMemoryNode(Node):
         if not self.x_coords:
             return False
         
-        distance_eucluiden = np.sqrt((x_get_time_step - self.x_coords)**2 + 
-                                     (y_get_time_step - self.y_coords)**2 + 
-                                     (z_get_time_step - self.z_coords)**2)
-        return np.any(distance_eucluiden < self.duplicate_threshold)
+        distance_euclideen = np.sqrt(
+            (x_get_time_step - np.array(self.x_coords))**2 + 
+            (y_get_time_step - np.array(self.y_coords))**2 + 
+            (z_get_time_step - np.array(self.z_coords))**2
+        )
+        return np.any(distance_euclideen < self.duplicate_threshold)
 
     def publish_obstacle_memory(self):
         """
@@ -57,18 +59,24 @@ class ObstacleMemoryNode(Node):
         obstacle_msg.float_storage  = [val for sublist in zip(self.x_coords, self.y_coords, self.z_coords) for val in sublist]
         self.memory_publisher.publish(obstacle_msg)
         
-        arraye_format = np.array(obstacle_msg.float_storage).reshape(-1, 3)
+        float_array = np.array(obstacle_msg.float_storage)
+        if float_array.size % 3 == 0:
+            arraye_format = float_array.reshape(-1, 3)
+        else:
+            self.get_logger().error('Obstacle memory storage has incorrect shape!')
+            return
+
         for i, obs_coords in enumerate(arraye_format): 
             topic_name = f'/obstacle_position_{i}'
-            if topic_name not in self.publishers:
-                self.publishers[topic_name] = self.create_publisher(PointStamped, topic_name, 10)
+            if topic_name not in self.obstacle_publishers:
+                self.obstacle_publishers[topic_name] = self.create_publisher(PointStamped, topic_name, 10)
             
             PS = PointStamped()
             PS.header.stamp = self.get_clock().now().to_msg()
             PS.point.x, PS.point.y, PS.point.z = obs_coords
 
             try:
-                self.publishers[topic_name].publish(PS)
+                self.obstacle_publishers[topic_name].publish(PS)
             except Exception as e:
                 self.get_logger().error(f'Error publishing obstacle to topic {topic_name}: {e}')           
 
